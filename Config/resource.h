fd3fa4399d322c0a6ea3f3e14435ae736eb5621f
wking <mail@wkings.net>
2024-02-29 16:18:40 +0800

    修改界面控件ID

--- Config/resource.h
+++ Config/resource.h
@@ -3,6 +3,31 @@
 // π© Config.rc  π”√
 //
 #define IDD_DIALOG_CONFIG               101
+#define IDC_EDIT1                       1002
+#define IDC_EDIT_DIR                    1002
+#define IDC_EDIT_PATH                   1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON_SELECT_PATH          1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT_URL                    1004
+#define IDC_BUTTON3                     1005
+#define IDC_BUTTON_ADDDIR               1005
+#define IDC_BUTTON4                     1006
+#define IDC_BUTTON_ADDFILE              1006
+#define IDC_BUTTON5                     1007
+#define IDC_BUTTON_DELETE               1007
+#define IDC_LIST1                       1008
+#define IDC_LIST_FILES                  1008
+#define IDC_BUTTON6                     1009
+#define IDC_BUTTON_LOAD                 1009
+#define IDC_BUTTON7                     1010
+#define IDC_BUTTON7_SAVE                1010
+#define IDC_BUTTON8                     1011
+#define IDC_BUTTON_XML                  1011
+#define IDC_CHECK1                      1012
+#define IDC_CHECK_EXEC                  1012
+#define IDC_CHECK2                      1013
+#define IDC_CHECK_UNZIP                 1013
 
 // Next default values for new objects
 // 
@@ -10,7 +35,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
