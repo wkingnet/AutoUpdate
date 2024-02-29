d8d83356c907e02c3941e630f8ac6385f2644f60
wking <mail@wkings.net>
2024-02-29 15:07:20 +0800

    添加项目文件。

---
+++ Config/header.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <Windows.h>
+#include <Windowsx.h>
+
+
+// 开启可视化效果
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+INT_PTR CALLBACK ProcConfig(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
---
+++ Config/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "header.h"
+#include "resource.h"
+
+HINSTANCE hgInst;
+int WINAPI WinMain(_In_  HINSTANCE hInstance,
+                   _In_opt_  HINSTANCE hPrevInstance,
+                   _In_  LPSTR lpCmdLine,
+                   _In_  int nCmdShow) {
+  hgInst = hInstance;
+  HWND hdlg = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG_CONFIG), GetDesktopWindow(), (DLGPROC)ProcConfig);
+  if (!hdlg) {
+    return 0;
+  }
+  ShowWindow(hdlg, SW_SHOW);
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return 0;
+}
+
+INT_PTR CALLBACK ProcConfig(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+  switch (msg) {
+  case WM_INITDIALOG:
+    // 设置对话框的图标
+    //SendMessage(hDlg, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(hgInst, MAKEINTRESOURCE(IDI_APP)));
+    return 0;
+  case WM_SYSCOMMAND:
+    if (wParam == SC_CLOSE) {
+      PostQuitMessage(0);//退出
+    }
+    return 0;
+  }
+  return (INT_PTR)FALSE;
+}
